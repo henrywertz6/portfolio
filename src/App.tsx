a00ea86fd238c9f23b45fa41c93e0023d545be45
Henry Wertz <hwertz@princeton.edu>
2024-08-27 14:26:26 -0400

    Fix build errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Projects from "./components/Projects";
-import Contact from "./components/Contact";
 
 function App() {
   return (
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { projects } from "../data";
 
 interface Project {
