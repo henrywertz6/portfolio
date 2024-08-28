6d0d4848336e14d1889eb51cb771964a7c16e4cd
Henry Wertz <hwertz@princeton.edu>
2024-08-27 20:22:41 -0400

    Short personal description added

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,21 +5,20 @@ export default function About() {
         Hi, I'm Henry.
         <br className="hidden lg:inline-block" />
       </h1>
-      <p className="mb-8 leading-relaxed">
-        {/* Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui laborum
-        quasi, incidunt dolore iste nostrum cupiditate voluptas? Laborum,
-        voluptas natus? */}
+      <p className="mb-8 leading-relaxed text-center">
+        I recently graduated from Princeton University with a degree in Computer
+        Science.
       </p>
       <div className="flex justify-center">
         <a
           href="https://www.linkedin.com/in/henry-wertz-09938b178/"
-          className="inline-flex text-[#ECDFCC] bg-[#697565] border-0 py-2 px-6 focus:outline-none hover:bg-[#6f7c6a] rounded text-lg"
+          className="inline-flex text-[#ECDFCC] bg-[#697565] border-0 py-2 px-6 focus:outline-none hover:bg-[#6f7c6a] rounded text-lg text-center"
         >
           Work With Me
         </a>
         <a
           href="https://github.com/henrywertz6"
-          className="ml-4 inline-flex text-[#ECDFCC] bg-[#3C3D37] border-0 py-2 px-6 focus:outline-none hover:bg-[#44463f] rounded text-lg"
+          className="ml-4 inline-flex text-[#ECDFCC] bg-[#3C3D37] border-0 py-2 px-6 focus:outline-none hover:bg-[#44463f] rounded text-lg text-center"
         >
           See My Past Work
         </a>
