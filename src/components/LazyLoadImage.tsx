35e84ec92b7bd304b8806fda4070dc9b95dd0d67
Henry Wertz <hwertz@princeton.edu>
2024-09-04 15:55:06 -0400

    Image optimization

---
+++ src/components/LazyLoadImage.tsx
@@ -0,0 +1,19 @@
+interface LazyLoadImageProps {
+  src: string;
+  alt: string;
+  className?: string;
+  width?: number | string;
+  height?: number | string;
+}
+
+const LazyLoadImage: React.FC<LazyLoadImageProps> = ({
+  src,
+  alt,
+  className,
+  width,
+  height,
+}) => {
+  return <div></div>;
+};
+
+export default LazyLoadImage;
