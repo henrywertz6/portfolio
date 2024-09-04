e0b08faf69a0f3ffda173fac203732e9d981ffb5
Henry Wertz <hwertz@princeton.edu>
2024-09-04 16:05:18 -0400

    Removed in progress file

--- src/components/LazyLoadImage.tsx
+++ src/components/LazyLoadImage.tsx
@@ -1,19 +1,19 @@
-interface LazyLoadImageProps {
-  src: string;
-  alt: string;
-  className?: string;
-  width?: number | string;
-  height?: number | string;
-}
+// interface LazyLoadImageProps {
+//   src: string;
+//   alt: string;
+//   className?: string;
+//   width?: number | string;
+//   height?: number | string;
+// }
 
-const LazyLoadImage: React.FC<LazyLoadImageProps> = ({
-  src,
-  alt,
-  className,
-  width,
-  height,
-}) => {
-  return <div></div>;
-};
+// const LazyLoadImage: React.FC<LazyLoadImageProps> = ({
+//   src,
+//   alt,
+//   className,
+//   width,
+//   height,
+// }) => {
+//   return <div></div>;
+// };
 
-export default LazyLoadImage;
+// export default LazyLoadImage;
